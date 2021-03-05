14592a35355241de7076bd3558abce17e25b5edd
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-05 09:09:43 +0530

    initial commit

---
+++ factorial.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int p,q=1,a;;
+	cin>>p;
+	while(p>0)
+    {
+
+
+        a=q*p;
+        q=a;
+         p--;
+
+    }
+    cout<<a<<endl;
+
+	system("pause>0");
+}
+
+
+
+
